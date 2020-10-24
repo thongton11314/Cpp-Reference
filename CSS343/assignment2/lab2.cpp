ad3ae823924e4e8758735effb2bb309939851a1c
Thong Ton <tonthong@csslab1.uwb.edu>
2020-10-23 17:05:01 -0700

    A

--- CSS343/assignment2/lab2.cpp
+++ CSS343/assignment2/lab2.cpp
@@ -63,12 +63,12 @@ int main() {
 		cout << "Retrieve --> sss:  " << (found ? "found" : "not found") << endl;
 
 		// test getHeight 
-		cout << "Height    --> and:  " << T.getHeight(andND) << endl;
-		cout << "Height    --> not:  " << T.getHeight(notND) << endl;
-		cout << "Height    --> sss:  " << T.getHeight(sssND) << endl;
-		cout << "Height    --> tttt: " << T.getHeight(ttttND) << endl;
-		cout << "Height    --> ooo:  " << T.getHeight(oooND) << endl;
-		cout << "Height    --> y:    " << T.getHeight(yND) << endl;
+		cout << "Height   --> and:  " << T.getHeight(andND) << endl;
+		cout << "Height   --> not:  " << T.getHeight(notND) << endl;
+		cout << "Height   --> sss:  " << T.getHeight(sssND) << endl;
+		cout << "Height   --> tttt: " << T.getHeight(ttttND) << endl;
+		cout << "Height   --> ooo:  " << T.getHeight(oooND) << endl;
+		cout << "Height   --> y:    " << T.getHeight(yND) << endl;
 
 		// test ==, and != 
 		T2 = T;
